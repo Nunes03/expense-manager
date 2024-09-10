10e077b59a966071bc28b52eb87e4e0a82acbcb0
Lucas Nunes <lucasnunes.ln365@gmail.com>
2024-09-09 23:23:20 -0300

    first commit

---
+++ src/main/java/nunes03/com/github/expensemanager/ExpenseManagerApplication.java
@@ -0,0 +1,12 @@
+package nunes03.com.github.expensemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerApplication.class, args);
+    }
+}
